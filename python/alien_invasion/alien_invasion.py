2147bea8b3e3188857b1bc488df3ad173a318e93
chenhuamin9527 <359247454@qq.com>
2020-08-07 17:03:54 +0800

    daily practise

--- python/alien_invasion/alien_invasion.py
+++ python/alien_invasion/alien_invasion.py
@@ -2,7 +2,9 @@ import pygame
 from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
+from button import Button
 import game_functions as gf
+from game_stats import GameStats
 
 
 def run_game():
@@ -12,6 +14,12 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建Play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    # 创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
     # 创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings, screen)
     bullets = Group()
@@ -21,12 +29,16 @@ def run_game():
 
     # 开始游戏的主循环
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_bullets(bullets)
-        gf.update_aliens(aliens)
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            bullets.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets, play_button)
 
 
 run_game()
+# 279页
--- python/alien_invasion/settings.py
+++ python/alien_invasion/settings.py
@@ -9,9 +9,10 @@ class Settings():
         self.bg_color = (255, 255, 255)
         # 飞船的设置
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
         # 子弹设置
-        self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_speed_factor = 3
+        self.bullet_width = 2
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
