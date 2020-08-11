97d49c7f2ce504b6e7f998ba78d1377a522a5df1
chenhuamin9527 <359247454@qq.com>
2020-08-11 15:27:33 +0800

    delete

--- C_C++/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projects/C_C++
-# Build directory: D:/projects/C_C++/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
